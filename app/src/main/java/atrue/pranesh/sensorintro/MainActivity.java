da4c916f65f671c8471c4d3d0eca27be34569b5c
pranesh <pranesh.msc@gmail.com>
2017-10-11 12:52:46 +0530

    Accelerometer Added

--- app/src/main/java/atrue/pranesh/sensorintro/MainActivity.java
+++ app/src/main/java/atrue/pranesh/sensorintro/MainActivity.java
@@ -1,13 +1,47 @@
 package atrue.pranesh.sensorintro;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    SensorManager sensorManager;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=(TextView)findViewById(R.id.txtDisplay);
+        sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
+        List list=sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
+
+        if(list.size()>0){
+        sensorManager.registerListener(new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float[] values = event.values;
+                textView.setText("x: "+values[0]+"\ny: "+values[1]+"\nz: "+values[2]);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        }, (Sensor) list.get(0),SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else{
+            Toast.makeText(getBaseContext(), "Error: No Accelerometer.", Toast.LENGTH_LONG).show();
+        }
+
+
+
+
     }
 }
