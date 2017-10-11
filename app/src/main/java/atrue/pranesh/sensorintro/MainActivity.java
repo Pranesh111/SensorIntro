4d6f35c10cf0b3144f3e004399c207b74104aee0
pranesh <pranesh.msc@gmail.com>
2017-10-11 13:58:46 +0530

    Accelerometer color changes in Button

--- app/src/main/java/atrue/pranesh/sensorintro/MainActivity.java
+++ app/src/main/java/atrue/pranesh/sensorintro/MainActivity.java
@@ -1,47 +1,121 @@
 package atrue.pranesh.sensorintro;
 
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     SensorManager sensorManager;
     TextView textView;
+    Button btnCompass;
+    Button btnAccelerometer;
+    List list;
+    private boolean color = false;
+    private long lastUpdate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView=(TextView)findViewById(R.id.txtDisplay);
-        sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
-        List list=sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
 
-        if(list.size()>0){
-        sensorManager.registerListener(new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                float[] values = event.values;
-                textView.setText("x: "+values[0]+"\ny: "+values[1]+"\nz: "+values[2]);
-            }
+        textView = (TextView) findViewById(R.id.txtDisplay);
+        btnCompass = (Button) findViewById(R.id.btnCompass);
+        btnAccelerometer = (Button) findViewById(R.id.btnAccelerometer);
 
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        list = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
 
-            }
-        }, (Sensor) list.get(0),SensorManager.SENSOR_DELAY_NORMAL);
-        }
-        else{
+        if (list.size() > 0) {
+            sensorManager.registerListener(sensorEventListener, (Sensor) list.get(0), SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
             Toast.makeText(getBaseContext(), "Error: No Accelerometer.", Toast.LENGTH_LONG).show();
         }
 
+        btnCompass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
 
+     
+    }
+
+    SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float[] values = event.values;
+            textView.setText("x: " + values[0] + "\ny: " + values[1] + "\nz: " + values[2]);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (list.size() > 0) {
+            sensorManager.unregisterListener(sensorEventListener);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            getAccelerometer(event);
+        }
+    }
+
+    private void getAccelerometer(SensorEvent event) {
+        float[] values = event.values;
+        // Movement
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+
+        float accelationSquareRoot = (x * x + y * y + z * z)
+                / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+        long actualTime = event.timestamp;
+        if (accelationSquareRoot >= 2) //
+        {
+            if (actualTime - lastUpdate < 200) {
+                return;
+            }
+            lastUpdate = actualTime;
+            Toast.makeText(this, "Device was shuffed", Toast.LENGTH_SHORT)
+                    .show();
+            if (color) {
+                btnAccelerometer.setBackgroundColor(Color.GREEN);
+            } else {
+                btnAccelerometer.setBackgroundColor(Color.RED);
+            }
+            color = !color;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 }
